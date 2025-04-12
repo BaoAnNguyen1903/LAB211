e604201ee6f842c11be16e8c950dd25b9de702a4
An Nguyen <anndb1903@gmail.com>
2025-04-12 23:41:42 +0700

    project 3

---
+++ ExpenseManage/src/view/Main.java
@@ -0,0 +1,9 @@
+import controller.Programming;
+
+public class Main {
+    public static void main(String[] args) {
+        String mChon[] = {"Add an expense","Display all expenses","Delete an expense", "Quit"};
+        Programming program = new Programming("=======Handy Expense program======", mChon);
+        program.run();
+    }
+}
